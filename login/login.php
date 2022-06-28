a59306b9e2d3296cd436e92ad106ec8b4238fa27
Kennedy Ryan <69519860+xOrpheusx@users.noreply.github.com>
2022-06-28 10:35:59 -0300

    Alterações e atualizações

---
+++ login/login.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/login/styleLogin.css">
+    
+
+
+    <title>Login</title>
+</head>
+<body>
+
+    <main>
+
+
+        <form action="" method="get">
+
+            <div class="login">
+                <h1>Login</h1>
+
+                <label for="email">E-mail</label>
+                <input class="email" type="email" name="email" id="email">
+
+                <label for="password">Senha</label>
+                <input type="password" name="senha" id="senha">
+
+                <input type="submit" value="entrar">
+            </div>
+        </form>
+
+    </main>
+</body>
+</html>
