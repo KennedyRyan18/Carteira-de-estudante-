564d238556bee318cc1e076f3575836337ac660e
Kennedy Ryan <69519860+xOrpheusx@users.noreply.github.com>
2022-07-04 14:36:22 -0300

    Update dados.php

--- public/login/dados.php
+++ public/login/dados.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina restrita</title>
-</head>
-<body>
+<?php
+session_start();
 
-<h1>Pagina restrita</h1>
-    
-</body>
-</html>
+?>
