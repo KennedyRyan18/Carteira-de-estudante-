e19d607da40806bb390881c155f415ad8fb9fbe2
Kennedy Ryan <69519860+xOrpheusx@users.noreply.github.com>
2022-07-04 09:16:43 -0300

    atualizando o css do cadastro.php

--- CarteirinhaCadastro/cadastro.php
+++ CarteirinhaCadastro/cadastro.php
@@ -3,6 +3,8 @@
 
     //session
     session_start();
+    
+        
 
     //coleta de dados
     $instituição = @$_POST['insti'];
@@ -21,8 +23,14 @@
 
     //tabela login
 
-    $sql2 = "INSERT INTO tblogin (email, senha) VALUES ('$email', '$senha')";
-    $rs2 = mysqli_query($connect, $sql2);
+    if( strlen( $email ) > 0 && strlen($senha) > 0 )
+    {  
+      $sql2 = "INSERT INTO tblogin (email, senha ) values ( '$email', '$senha' )";
+      $rs2 = mysqli_query($connect, $sql2);
+    }
+
+    // $sql2 = "INSERT INTO tblogin (email, senha) VALUES ('$email', '$senha')";
+    // $rs2 = mysqli_query($connect, $sql2);
 
 
     // tabela estudante
@@ -30,25 +38,26 @@
     $sql3 = "INSERT INTO tbestudante (cpf, matricula, nome) VALUES ('$cpf', '$matricula', '$nome')";
     $rs3 = mysqli_query($connect, $sql3);
 
-    if ($rs1 && $rs2 && $rs3) {
-        echo " cliente registrado";
-    }else {
-        echo "erro";
-    }
+   
 
-    
-    
+        //alertas
 
-        // if ($connect->query($sql) === TRUE) {
-        // echo "New record created successfully";
-        // } else {
-        // echo "Error: " . $sql . "<br>" . $connect->error;
-        // }
+        if (isset($_POST['btn'])):
+            $aviso = array();
+            $erros = array();
+            $email = mysqli_escape_string($connect, $_POST['Email']);
+            $senha = mysqli_escape_string($connect, $_POST['senha']);
+            $instituição = mysqli_escape_string($connect, $_POST['insti']);
+            $matricula =  mysqli_escape_string($connect, $_POST['matricula']);
+            $nome = mysqli_escape_string($connect, $_POST['nome']);
+            $cpf = mysqli_escape_string($connect, $_POST['cpf']);
+            
+            if(empty($email) or empty($senha) or empty($instituição) or empty($matricula) or empty($nome) or empty($cpf)):
+                $erros[] = "<li class = alerta> O cadastro precisa ser preenchido </li>";
+            endif;
+        endif;
 
-        // $connect->close();
-
-
-
+        
     
 ?>
 
@@ -60,7 +69,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
-    <link rel="stylesheet" href="/css/cadastro.css">
+    <link rel="stylesheet" href="stylecadastro.php">
     <title>Cadastro</title>
 
          <!-- mascara para cpf -->
@@ -86,53 +95,89 @@
 </head>
 <body>
 
+    <header>
+        <div class="logo">
+            <h2>Carteirinha</h2>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li><a href="#">Duvidas?</a></li>
+                <li><a href="#">Email</a></li>
+                <li class="login"><a href="/login/index.html">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
     <main>
+
+      
        
         <form class="" method="POST">
 
-            <h1>Cadastro da carteirinha</h1>
+            
+            <div class="cadastro"> 
+            <?php
+             if ($rs1 && $rs2 && $rs3):
+                echo "<li class = alerta> Casdastro concluido, Muito Obrigueido </li>";
+            endif;
+           
+            ?>
+            <?php
+             if(!empty($erros)):
+                foreach ($erros as $erro):
+                     echo $erro;
+                endforeach;
+             endif;
 
-            <div class="carteirinha"> 
+            
+            ?>
+                <h1>Cadastro da carteirinha</h1>
 
-                <div>
-                <label for="text">Intituição educacional</label>
-                <br>
-                <input type="text" name="insti" id="insti">
+                <div class="NomeCpf">
+                    <div class="nome">
+                        <label for="text">Nome</label>
+                        <br>
+                        <input type="text" name="nome" id="nome">
+                    </div>
+
+                    <div class="cpf">
+                        <label for="text">CPF</label>
+                        <br>
+                        <input type="text" name="cpf" id="cpf" oninput="mascara(this)"> 
+                     </div>
+                </div>
+                
+                <div class="InstMatricula">
+                    <div class="inst">
+                        <label for="text">Intituição educacional</label>
+                        <br>
+                        <input type="text" name="insti" id="insti">
+                    </div>
+
+                    <div class="matricula">
+                        <label for="number">matricula</label>
+                        <br>
+                        <input type="text" id="matricula" name="matricula" maxlength="11">
+                    </div>
+                </div>
+
+                <div class="EmailSenha">
+                     <div class="email">
+                        <label for="email">Email</label>
+                        <br>
+                        <input type="email" name="Email" id="Email" require = "required">
+                    </div>
+
+                    <div class="senha">
+                        <label for="password">Senha</label>
+                        <br>
+                        <input type="password" name="senha" id="myInput" maxlength="10">
+                    </div>
                 </div>
 
                 <div>
-                    <label for="text">Nome</label>
-                    <br>
-                    <input type="text" name="nome" id="nome">
+                    <input type="submit" class="cadastrar" name="btn" value="cadastrar" />
                 </div>
-
-                <div>
-                <label for="text">CPF</label>
-                <br>
-                    <input type="text" name="cpf" id="cpf" oninput="mascara(this)"> 
-                </div>
-
-            <div>
-                <label for="number">matricula</label>
-                <br>
-                <input type="text" id="matricula" name="matricula" maxlength="11">
-            </div>
-
-            <div>
-                <label for="email">Email</label>
-                <br>
-                <input type="email" name="Email" id="Email">
-            </div>
-
-            <div class="senha">
-                <label for="password">Senha</label>
-                <br>
-               <input type="password" name="senha" id="myInput" maxlength="10">
-            </div>
-
-            <div>
-                <input type="submit" value="Cadastrar">
-            </div>
             </div>
 
             
--- CarteirinhaCadastro/conexao.php
+++ CarteirinhaCadastro/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "sistemacarteirinha";
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "b201ddc6abe7ea";
+$password = "05d3598d";
+$db_name = "heroku_28d4118fc6643ce";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 
